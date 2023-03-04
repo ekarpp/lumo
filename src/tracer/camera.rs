f57f6a99b91569a386215da35810be657d5ce858
ekarpp <eetu@karppinen.xyz>
2023-03-04 21:50:04 +0200

    improve camera

--- src/tracer/camera.rs
+++ src/tracer/camera.rs
@@ -2,39 +2,39 @@ use glam::f64::DVec3;
 use crate::tracer::ray::Ray;
 
 pub struct Camera {
-    vp_height: f64,
-    vp_width: f64,
     origin: DVec3,
-    pub bot_left_corner: DVec3
+    horiz: DVec3,
+    vert: DVec3,
+    /* bottom left corner */
+    blc: DVec3
 }
 
 impl Camera {
     pub fn ray_at(&self, u: f64, v: f64) -> Ray {
         Ray {
             origin: self.origin,
-            dir: self.bot_left_corner - self.origin
-                + DVec3::new(
-                    u*self.vp_width,
-                    v*self.vp_height,
-                    0.0
-                )
+            dir: self.blc + u*self.horiz + v*self.vert - self.origin
         }
     }
-}
 
-pub fn default() -> Camera {
-    /* viewport height */
-    let h = 2.0;
-    /* viewport width */
-    let w = h * 16.0 / 9.0;
-    /* focal length */
-    let f = 1.0;
-    let origin = DVec3::ZERO;
+    pub fn new(ar: f64, from: DVec3, towards: DVec3, up: DVec3) -> Camera {
+        /* viewport height */
+        let vph = 2.0;
+        /* viewport width */
+        let vpw = vph * ar;
 
-    Camera {
-        vp_height: h,
-        vp_width: w,
-        origin: origin,
-        bot_left_corner: origin - 0.5*DVec3::new(w, h, 2.0*f)
+        let w = (from - towards).normalize();
+        let u = up.cross(w).normalize();
+        let v = w.cross(u);
+
+        let horiz = u * vpw;
+        let vert = v * vph;
+
+        Camera {
+            origin: from,
+            horiz: horiz,
+            vert: vert,
+            blc: from - (horiz + vert) / 2.0 - w
+        }
     }
 }
