e1de01042178b094e2c637476285900959247aa6
ekarpp <eetu@karppinen.xyz>
2023-03-05 00:38:36 +0200

    slight change

--- src/tracer/material.rs
+++ src/tracer/material.rs
@@ -12,8 +12,10 @@ pub enum Material {
 
 impl Material {
     pub fn shade(&self, h: &Hit) -> DVec3 {
+        let q = 3.0;
+        let spec_coeff = DVec3::splat(0.9);
         match self {
-            Material::Default(c) => phong_illum(c.clone(), h, DVec3::splat(0.9), 3.0),
+            Material::Default(c) => phong_illum(c.clone(), h, spec_coeff, q),
             _ => DVec3::ZERO,
         }
     }
