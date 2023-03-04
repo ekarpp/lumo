9961b31fc6f0e9c4526d5d598e7fd58cb3a3dcb7
ekarpp <eetu@karppinen.xyz>
2023-03-04 02:36:45 +0200

    add trivial sphere support to tracer

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,12 @@ fn main() {
     };
 
     let cam = tracer::camera::def();
-
+    let scene = vec![
+        tracer::sphere::Sphere{
+            origin: glam::f32::Vec3::new(0.0, -100.5, -1.0),
+            radius: 100.0
+        }
+    ];
     for y in (0..HEIGHT).rev()
     {
         for x in 0..WIDTH
@@ -22,7 +27,7 @@ fn main() {
             let u = x as f32 / (WIDTH-1) as f32;
             let v = y as f32 / (HEIGHT-1) as f32;
             let r = cam.ray_at(u, v);
-            image.buffer.push(r.color());
+            image.buffer.push(r.color(&scene));
         }
     }
     image.save();
--- src/tracer/camera.rs
+++ src/tracer/camera.rs
@@ -5,15 +5,19 @@ pub struct Camera {
     vp_height: f32,
     vp_width: f32,
     origin: Vec3,
-    bot_left_corner: Vec3
+    pub bot_left_corner: Vec3
 }
 
 impl Camera {
-    pub fn ray_at(&self, x: f32, y: f32) -> Ray {
+    pub fn ray_at(&self, u: f32, v: f32) -> Ray {
         Ray {
             origin: self.origin,
-            dir: self.bot_left_corner
-                + Vec3::new(x*self.vp_width, y*self.vp_height, 0.0) - self.origin
+            dir: self.bot_left_corner - self.origin
+                + Vec3::new(
+                    u*self.vp_width,
+                    v*self.vp_height,
+                    0.0
+                )
         }
     }
 }
@@ -27,6 +31,6 @@ pub fn def() -> Camera {
         vp_height: h,
         vp_width: w,
         origin: Vec3::ZERO,
-        bot_left_corner: Vec3::ZERO - 0.5*Vec3::new(w, h, 2.0*f)
+        bot_left_corner: -0.5*Vec3::new(w, h, 2.0*f)
     }
 }
