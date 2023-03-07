80e43d7b36f832925a259f1d0c5f1e1118e59360
ekarpp <eetu@karppinen.xyz>
2023-03-07 16:50:49 +0200

    add tests to triangle

--- src/tracer/object/triangle_tests.rs
+++ src/tracer/object/triangle_tests.rs
@@ -1,11 +1,42 @@
-/*
+use super::*;
 
-this order no hit, change point order and hit
+fn get_mat() -> Material {
+    Material::Phong(crate::tracer::texture::Texture::Solid(DVec3::ONE))
+}
 
-Triangle::new(
-DVec3::new(-0.3, -0.3, -1.0),
-DVec3::new(0.3, -0.3, -1.0),
-DVec3::new(0.0, 0.0, -1.0),
-Material::Phong(Texture::Solid(DVec3::ONE)),
-                ),
-*/
+#[test]
+fn point_order_irrelevant() {
+    let pts = vec![
+        DVec3::new(-0.3, -0.3, -1.0),
+        DVec3::new(0.3, -0.3, -1.0),
+        DVec3::new(0.0, 0.0, -1.0),
+    ];
+
+    let r = Ray::new(DVec3::ZERO, DVec3::new(0.0, 0.0, -1.0));
+
+    (0..6).for_each(|i| {
+        let a = i / 2;
+        let b = i % 2;
+
+        let t = Triangle::new(
+            /* permutations */
+            pts[a],
+            pts[(a + b + 1)%3],
+            pts[2 - (i % 3)],
+            get_mat()
+        );
+        assert!(t.hit(&r).is_some());
+    });
+}
+
+#[test]
+fn no_self_intersect() {
+    let t = Triangle::new(
+        DVec3::ZERO,
+        DVec3::ONE,
+        DVec3::new(1.0, 0.0, 1.0),
+        get_mat(),
+    );
+    let r = Ray::new(DVec3::ZERO, DVec3::new(0.0, 0.0, -1.0));
+    assert!(t.hit(&r).is_none());
+}
