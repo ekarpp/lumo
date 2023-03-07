78a5b6d84b701f5239e811d23ef3cf815d0ffc18
ekarpp <eetu@karppinen.xyz>
2023-03-07 11:17:53 +0200

    make checkerboard take textures instead of colors

--- src/tracer/texture.rs
+++ src/tracer/texture.rs
@@ -2,9 +2,13 @@ use crate::DVec3;
 use crate::perlin::Perlin;
 use Texture::*;
 
+const CHECKER_SCALE: f64 = 13.0;
+
 pub enum Texture {
     Solid(DVec3),
-    Checkerboard,
+    /* box avoids having to define lifetime all the way to objects.
+     * should texture be a struct instead? */
+    Checkerboard(Box<Texture>, Box<Texture>),
     Marble(Perlin),
 }
 
@@ -12,18 +16,19 @@ impl Texture {
     pub fn color_at(&self, p: DVec3) -> DVec3 {
         match self {
             Solid(c) => c.clone(),
-            Checkerboard => Self::checkers_at(p),
             Marble(pn) => pn.color_at(p),
+            Checkerboard(t1, t2) => {
+                if self.checkers_phase(p) > 0.0 {
+                    t1.color_at(p)
+                } else {
+                    t2.color_at(p)
+                }
+            }
         }
     }
 
-    fn checkers_at(p: DVec3) -> DVec3 {
-        // if looks bad, try to tune s
-        let s = 13.0;
-        if (s*p.x).sin() * (s*p.y).sin() * (s*p.z).sin() > 0.0 {
-            DVec3::ZERO
-        } else {
-            DVec3::ONE
-        }
+    fn checkers_phase(&self, p: DVec3) -> f64 {
+        let t = CHECKER_SCALE*p;
+        (t.x).sin() * (t.y).sin() * (t.z).sin()
     }
 }
