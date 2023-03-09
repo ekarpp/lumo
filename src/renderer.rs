4e3bf09cbfe6f8b47b8eac3db5fe519ccb7c7f45
ekarpp <eetu@karppinen.xyz>
2023-03-09 20:14:49 +0200

    use sampler in renderer

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,9 +1,11 @@
-use crate::DVec3;
-use crate::rand_utils;
+use crate::{DVec3, DVec2};
+use crate::samplers::UniformSampler;
 use crate::tracer::ray::Ray;
 use crate::tracer::scene::Scene;
 use crate::tracer::camera::Camera;
 use rayon::iter::{ParallelIterator, IntoParallelIterator};
+
+type PxSampler = UniformSampler;
 
 pub fn _render(
     img_height: usize,
@@ -19,10 +21,8 @@ pub fn _render(
             let u = x as f64 * px_width;
             let v = (img_height - 1 - y) as f64 * px_height;
 
-            (0..num_samples).map(|_| {
-                let randx = rand_utils::rand_f64();
-                let randy = rand_utils::rand_f64();
-                cam.ray_at(u + randx*px_width, v + randy*px_height)
+            PxSampler::new(num_samples).map(|rand_sq: DVec2| {
+                cam.ray_at(u + rand_sq.x*px_width, v + rand_sq.y*px_height)
             }).fold(DVec3::ZERO, |acc: DVec3, r: Ray| {
                 acc + r.color(scene)
             }) / num_samples as f64
