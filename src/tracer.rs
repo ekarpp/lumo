55009e198c988ab3f1195d9d27fc8c8dc8490d75
ekarpp <eetu@karppinen.xyz>
2023-03-12 11:50:13 +0200

    refactoring

--- src/tracer.rs
+++ src/tracer.rs
@@ -1,10 +1,9 @@
 pub mod hit;
 pub mod ray;
-pub mod brdfs;
+pub mod bxdfs;
 pub mod scene;
 pub mod camera;
 pub mod object;
 pub mod texture;
 pub mod material;
-pub mod scattering;
 pub mod integrators;
