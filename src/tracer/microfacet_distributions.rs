5801e3c23697c6e8b3f1d4303e76fcbb003bc9f3
ekarpp <eetu@karppinen.xyz>
2023-03-18 14:43:28 +0200

    add microfacet distribution

---
+++ src/tracer/microfacet_distributions.rs
@@ -0,0 +1,37 @@
+use crate::DVec3;
+use std::f64::consts::PI;
+
+pub enum MfDistribution {
+    Ggx(f64),
+}
+
+impl MfDistribution {
+    pub fn d(&self, wh: DVec3, no: DVec3) -> f64 {
+        match self {
+            Self::Ggx(roughness) => {
+                let wh_dot_no2 = wh.dot(no).powi(2);
+                let tan_wh = (1.0 - wh_dot_no2) / wh_dot_no2;
+                let roughness2 = roughness * roughness;
+
+                roughness2
+                    / (PI * (wh_dot_no2 * (roughness2 + tan_wh)).powi(2))
+            }
+        }
+    }
+
+    pub fn g(&self, wo: DVec3, no: DVec3) -> f64 {
+        1.0 / (1.0 + self.lambda(wo, no))
+    }
+
+    fn lambda(&self, wo: DVec3, no: DVec3) -> f64 {
+        match self {
+            Self::Ggx(roughness) => {
+                let wo_dot_no2 = wo.dot(no).powi(2);
+                let tan_wo = (1.0 - wo_dot_no2) / wo_dot_no2;
+                let roughness2 = roughness * roughness;
+
+                ((1.0 + roughness2 * tan_wo).sqrt() - 1.0) / 2.0
+            }
+        }
+    }
+}
