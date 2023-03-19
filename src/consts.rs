7c4568ee109a601e748c63f8105718ac3f27329b
ekarpp <eetu@karppinen.xyz>
2023-03-19 18:27:47 +0200

    reduce russian roulette probability. cleanup consts

--- src/consts.rs
+++ src/consts.rs
@@ -1,31 +1,13 @@
-use crate::DVec3;
-
 /// Epsilon to avoid self intersection of objects
-pub const EPSILON: f64 = 0.0001;
-
-/// number of shadow rays per intersection point. UNUSED
-#[allow(dead_code)]
-pub const SHADOW_RAYS: usize = 4*4;
+pub const EPSILON: f64 = 1e-10;
 
 /// Refraction constant of glass
 pub const ETA: f64 = 1.5;
 
-/// Maximum recursion depth of path tracing. UNUSED, russian roulette instead
-#[allow(dead_code)]
-pub const PATH_TRACE_MAX_DEPTH: usize = 5;
-
 /// Russian roulette probability for the path tracer.
 /// Terminates a path at each step with this probability.
 /// Computed values are multiplied by the reciprocal of the inverse probability.
-pub const PATH_TRACE_RR: f64 = 0.4;
-
-/// specular lobe coefficient. smaller = bigger lobe UNUSED
-#[allow(dead_code)]
-pub const LOBE_Q: f64 = 10.0;
-
-/// Intensity of the specular lobe UNUSED
-#[allow(dead_code)]
-pub const SPECULAR_COEFF: DVec3 = DVec3::splat(0.0);
+pub const PATH_TRACE_RR: f64 = 0.2;
 
 /*
  * TEXTURES
