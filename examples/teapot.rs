58e194c45bcfcf496da6b10bd1f63413737610c9
ekarpp <eetu@karppinen.xyz>
2023-03-23 00:07:16 +0200

    add teapot example

---
+++ examples/teapot.rs
@@ -0,0 +1,57 @@
+use rust_tracer::*;
+use std::fs::File;
+use std::io::Write;
+use std::io::Seek;
+use rust_tracer::tracer::integrator::Integrator;
+use rust_tracer::object::triangle::Triangle;
+use rust_tracer::object::kdtree::Mesh;
+use rust_tracer::object::instance::Instanceable;
+use rust_tracer::material::Material;
+use rust_tracer::texture::Texture;
+
+const FNAME_TMP: &str = "teapot.obj.tmp";
+const TEAPOT_URL: &str = "https://graphics.stanford.edu/courses/cs148-10-summer/as3/code/as3/teapot.obj";
+
+fn teapot() -> Result<Vec<Triangle>, Box<dyn std::error::Error>> {
+    let resp = ureq::get(TEAPOT_URL)
+        .call()?;
+    assert!(resp.has("Content-Length"));
+    let len: usize = resp.header("Content-Length")
+        .unwrap()
+        .parse()?;
+
+    let mut bytes: Vec<u8> = Vec::with_capacity(len);
+    resp.into_reader()
+        .read_to_end(&mut bytes)?;
+    let mut file = File::create(FNAME_TMP)?;
+    file.write_all(&bytes)?;
+    file.rewind()?;
+    let tringls = obj::load_obj_file(File::open(FNAME_TMP)?)?;
+
+    Ok(tringls)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let camera = Camera::default();
+    let mut scene = Scene::default();
+
+    scene.add(
+        Mesh::new(
+            teapot()?,
+            Material::diffuse(Texture::Solid(DVec3::new(0.0, 1.0, 0.0))),
+        )
+            .scale(DVec3::splat(0.1))
+            .rotate_y(3.14)
+            .translate(DVec3::new(0.0, -0.5, -1.5))
+            .make_box()
+    );
+
+
+    let mut renderer = Renderer::new(scene, camera);
+    renderer.set_width(640);
+    renderer.set_height(360);
+    renderer.set_integrator(Integrator::DirectLight);
+    renderer.render()
+        .save("def.png")?;
+    Ok(())
+}
