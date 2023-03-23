578c2a2a02e7d99f53f326166e2033ce2a18c09b
ekarpp <eetu@karppinen.xyz>
2023-03-23 14:48:07 +0200

    add bunny with more triangles

--- examples/bunny.rs
+++ examples/bunny.rs
@@ -1,21 +1,22 @@
 use rust_tracer::*;
 use std::fs::File;
-use std::io::{Seek, Write};
+use std::io::{Seek, Write, Cursor, Read};
+use zip::ZipArchive;
 
 const FNAME_TMP: &str = "bunny.obj.tmp";
-const BUNNY_URL: &str = "https://graphics.stanford.edu/~mdfisher/Data/Meshes/bunny.obj";
+const BUNNY_URL: &str = "https://www.prinmath.com/csci5229/OBJ/bunny.zip";
 
-fn teapot() -> Result<Vec<Triangle>, Box<dyn std::error::Error>> {
-    let resp = ureq::get(BUNNY_URL)
-        .call()?;
-    assert!(resp.has("Content-Length"));
-    let len: usize = resp.header("Content-Length")
-        .unwrap()
-        .parse()?;
-
-    let mut bytes: Vec<u8> = Vec::with_capacity(len);
-    resp.into_reader()
+fn bunny() -> Result<Vec<Triangle>, Box<dyn std::error::Error>> {
+    let mut bytes = Vec::new();
+    ureq::get(BUNNY_URL)
+        .call()?
+        .into_reader()
         .read_to_end(&mut bytes)?;
+
+    let mut zip = ZipArchive::new(Cursor::new(bytes))?;
+    let mut bytes = Vec::new();
+    zip.by_name("bunny.obj")?.read_to_end(&mut bytes)?;
+
     let mut file = File::create(FNAME_TMP)?;
     file.write_all(&bytes)?;
     file.rewind()?;
@@ -36,11 +37,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     scene.add(
         Mesh::new(
-            teapot()?,
+            bunny()?,
             Material::specular(Texture::Solid(DVec3::new(0.0, 1.0, 0.0)), 0.2),
         )
-            .scale(DVec3::splat(4.0))
-            .translate(DVec3::new(0.0, -1.1, -1.3))
+            .scale(DVec3::splat(0.1))
+        //.translate(DVec3::new(0.0, -1.1, -1.3))
+            .translate(DVec3::new(0.0, -0.5, -1.3))
             .make_box()
     );
 
