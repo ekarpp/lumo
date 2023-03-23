3b0a8a3724e460944376a4ddd3283571c9d36603
ekarpp <eetu@karppinen.xyz>
2023-03-23 23:48:07 +0200

    rewrite aabb intersection

--- src/tracer/object/aabb.rs
+++ src/tracer/object/aabb.rs
@@ -34,37 +34,16 @@ impl AaBoundingBox {
 
     /// Find `t_start` and `t_end` for ray intersection
     pub fn intersect(&self, r: &Ray) -> (f64, f64) {
-        let ro_min = (self.ax_min - r.origin).to_array();
-        let ro_max = (self.ax_max - r.origin).to_array();
-        let rd = r.dir.to_array();
+        let ro_min = (self.ax_min - r.origin) / r.dir;
+        let ro_max = (self.ax_max - r.origin) / r.dir;
 
-        // UNROLL LOOPP??
+        let t_start = ro_min.min(ro_max);
+        let t_end = ro_max.max(ro_min);
 
-        // axis parallel rays?
-        let mut t_start = -INFINITY;
-        let mut t_end = INFINITY;
-
-        for ax in 0..=2 {
-            if rd[ax].abs() < EPSILON {
-                if ro_min[ax] > 0.0 || ro_max[ax] < 0.0 {
-                    return (INFINITY, -INFINITY);
-                } else {
-                    continue;
-                }
-            }
-
-            let (mut t1, mut t2) =
-                (ro_min[ax] / rd[ax], ro_max[ax] / rd[ax]);
-
-            if t1 > t2 {
-                std::mem::swap(&mut t1, &mut t2);
-            }
-
-            t_start = t_start.max(t1);
-            t_end = t_end.min(t2);
-        }
-
-        (t_start, t_end)
+        (
+            t_start.max_element(),
+            t_end.min_element(),
+        )
     }
 
     /// Combine self and other to a new bigger AABB
