881a67b3af2e432ec2752f4764ddda9c61c2bfca
ekarpp <eetu@karppinen.xyz>
2023-03-23 13:12:45 +0200

    fix reflections in direct light integrator

--- src/tracer/integrator/direct_light.rs
+++ src/tracer/integrator/direct_light.rs
@@ -10,25 +10,36 @@ pub fn integrate(scene: &Scene, ro: &Ray) -> DVec3 {
             match material.bsdf_pdf(&ho, ro) {
                 None => material.emit(&ho),
                 Some(scatter_pdf) => {
-                    match material {
-                        Material::Diffuse(_) | Material::Microfacet(..) => {
-                            JitteredSampler::new(SHADOW_SPLITS)
-                                .fold(DVec3::ZERO, |acc, rand_sq| {
-                                    acc + shadow_ray(scene,
-                                                     ro,
-                                                     &ho,
-                                                     scatter_pdf.as_ref(),
-                                                     rand_sq)
-                                }) / SHADOW_SPLITS as f64
+                    if material.is_specular() {
+                        let no = ho.norm;
+                        let ri = scatter_pdf
+                            .sample_ray(rand_utils::unit_square());
+                        let wi = ri.dir;
 
-                        }
-                        Material::Glass | Material::Mirror => {
-                            let ri = scatter_pdf.sample_ray(
-                                rand_utils::unit_square());
-                            integrate(scene, &ri)
-                                / scatter_pdf.value_for(&ri)
-                        }
-                        _ => DVec3::ZERO,
+                        // tmp, find better way to do this.
+                        let tmp = matches!(material,
+                                           Material::Glass | Material::Mirror);
+
+                        let cos_theta = if tmp {
+                            1.0
+                        } else {
+                            no.dot(wi.normalize()).abs()
+                        };
+
+                        material.bsdf_f(ro, &ri, no)
+                            * cos_theta
+                            * integrate(scene, &ri)
+                            / scatter_pdf.value_for(&ri)
+                    } else {
+                        JitteredSampler::new(SHADOW_SPLITS)
+                            .fold(DVec3::ZERO, |acc, rand_sq| {
+                                acc + shadow_ray(scene,
+                                                 ro,
+                                                 &ho,
+                                                 scatter_pdf.as_ref(),
+                                                 rand_sq)
+                            }) / SHADOW_SPLITS as f64
+
                     }
                 }
             }
