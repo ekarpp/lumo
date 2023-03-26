300eb4782bbf5e1fa35947d810d4e05214a88ab4
ekarpp <eetu@karppinen.xyz>
2023-03-26 15:07:06 +0300

    modify teapot

--- examples/teapot.rs
+++ examples/teapot.rs
@@ -4,35 +4,33 @@ const TEAPOT_URL: &str = "https://graphics.stanford.edu/courses/cs148-10-summer/
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let camera = Camera::default();
-    let mut scene = Scene::default();
+    let def_color = srgb_to_lin(242, 242, 242);
+    let mut scene = Scene::empty_box(
+        def_color,
+        Material::diffuse(Texture::Solid(srgb_to_lin(255, 0, 0))),
+        Material::diffuse(Texture::Solid(srgb_to_lin(0, 255, 0))),
+        Material::diffuse(Texture::Solid(def_color)),
+    );
 
+    /*
+    let mut scene = Scene::default();
     scene.add(
         Sphere::new(
-            DVec3::ZERO,
-            10.0,
-            Material::diffuse(Texture::Solid(srgb_to_lin(0, 0, 0)))
+            DVec3::new(0.0, 1.0, -1.0),
+            0.2,
+            Material::Light(Texture::Solid(srgb_to_lin(255, 255, 255)))
         )
     );
-
-    scene.add(
-        Sphere::new(
-            DVec3::new(0.0, 0.5, -1.0),
-            0.25,
-            Material::Light(Texture::Solid(srgb_to_lin(255, 255, 255))),
-        ),
-    );
-
+     */
     scene.add(
         Mesh::new(
             obj_from_url(TEAPOT_URL)?,
-            Material::diffuse(Texture::Solid(srgb_to_lin(0, 255, 0))),
+            Material::diffuse(Texture::Solid(srgb_to_lin(0, 0, 255)))//Marble(Perlin::default()))
         )
             .scale(DVec3::splat(0.1))
-            .rotate_y(3.14)
-            .translate(DVec3::new(0.5, -0.5, -1.5))
+            .translate(DVec3::new(0.0, -0.5, -1.5))
             .make_box()
     );
-
 
     let renderer = Renderer::new(scene, camera);
     renderer.render()
