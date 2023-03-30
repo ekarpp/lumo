e459abec08656c2617fe4a97ec75de45e6791ee1
ekarpp <eetu@karppinen.xyz>
2023-03-30 23:41:59 +0300

    SAH fixes. still overflows

--- src/tracer/object/kdtree.rs
+++ src/tracer/object/kdtree.rs
@@ -65,8 +65,8 @@ impl<T: Bounded> KdTree<T> {
         aabb: &AaBoundingBox,
     ) -> Option<Hit> {
         // extract split info or check for hit at leaf node
-        let (axis, median, mut node_first, mut node_second) = match node {
-            KdNode::Split(axis, median, left, right) => (*axis, *median, left, right),
+        let (axis, point, mut node_first, mut node_second) = match node {
+            KdNode::Split(axis, point, left, right) => (*axis, *point, left, right),
             KdNode::Leaf(indices) => {
                 let mut tt = t_max;
                 let mut h = None;
@@ -81,11 +81,11 @@ impl<T: Bounded> KdTree<T> {
             }
         };
 
-        let t_split = (median - r.o(axis)) / r.d(axis);
+        let t_split = (point - r.o(axis)) / r.d(axis);
 
-        let (mut aabb_first, mut aabb_second) = aabb.split(axis, median);
+        let (mut aabb_first, mut aabb_second) = aabb.split(axis, point);
 
-        let left_first = r.o(axis) < median || (r.o(axis) == median && r.d(axis) <= 0.0);
+        let left_first = r.o(axis) < point || (r.o(axis) == point && r.d(axis) <= 0.0);
         // intersect first the AABB that we reach first
         if !left_first {
             std::mem::swap(&mut aabb_first, &mut aabb_second);
@@ -230,17 +230,17 @@ impl KdNode {
             // iterator for maxs
             let mut j = 0;
 
-            // add infinity to end of both so last element always loses comparison
+            // add infinity to end as "null"
             mins.push(INFINITY);
             maxs.push(INFINITY);
 
             // do quasi merge
-            while i < mins.len() - 1 || j < maxs.len() - 1 {
+            while mins[i] < INFINITY || maxs[i] < INFINITY {
                 let is_min = mins[i] <= maxs[j];
                 let point = mins[i].min(maxs[j]);
 
                 // update objects on right before cost..
-                if is_min { i += 1; num_right -= 1; }
+                if !is_min { j += 1; num_right -= 1; }
 
                 let cost = Self::cost(&boundary, axis, point, num_left, num_right);
 
@@ -251,7 +251,7 @@ impl KdNode {
                 }
 
                 // ..and objects on left after cost
-                if !is_min { j += 1; num_left += 1; }
+                if is_min { i += 1; num_left += 1; }
             }
         }
 
@@ -303,125 +303,5 @@ impl KdNode {
                 Self::construct(objects, bounds, right),
             ))
         }
-
-
-
-/*
-        /* ############################## */
-
-        if indices.len() < 16 {
-            return Box::new(Self::Leaf(indices));
-        }
-
-        // filter relevant AABBs
-        let aabbs: Vec<&AaBoundingBox> = indices.iter().map(|idx| &bounds[*idx]).collect();
-
-        // get AABB min/max values for each axis
-        let mut bb_vals: [Vec<f64>; 3] = Default::default();
-        aabbs.iter().for_each(|aabb| {
-            let mx = aabb.ax_max.to_array();
-            let mn = aabb.ax_min.to_array();
-            for ax in 0..3 {
-                bb_vals[ax].push(mn[ax]);
-                bb_vals[ax].push(mx[ax]);
-            }
-        });
-
-        // sort AABB min/max values
-        (0..3).for_each(|ax| {
-            bb_vals[ax].sort_by(|a: &f64, b: &f64| a.partial_cmp(b).unwrap());
-        });
-
-        let median = |svec: &[f64]| -> f64 {
-            if svec.len() % 2 == 1 {
-                svec[svec.len() / 2]
-            } else {
-                let mid = svec.len() / 2;
-                (svec[mid] + svec[mid - 1]) / 2.0
-            }
-        };
-
-        // find median among each axis
-        let med: Vec<f64> = (0..3).map(|ax| median(&bb_vals[ax])).collect();
-
-        let score = |axis: usize, median: f64| -> usize {
-            let mut left = 0;
-            let mut right = 0;
-            aabbs.iter().for_each(|aabb| {
-                if aabb.ax_min.to_array()[axis] <= median {
-                    left += 1;
-                }
-                if aabb.ax_max.to_array()[axis] >= median {
-                    right += 1;
-                }
-            });
-            left.max(right)
-        };
-
-        // score each axis by how well objects are split between the medians
-        let scores: Vec<usize> = (0..3).map(|ax| score(ax, med[ax])).collect();
-        let threshold = (indices.len() as f64 * THRESHOLD_MULTIPLIER) as usize;
-
-        // no good splits, make it a leaf
-        if scores[0].min(scores[1].min(scores[2])) >= threshold {
-            return Box::new(Self::Leaf(indices));
-        }
-
-        let mut split_axis = 3;
-        let curr_bounds = aabbs
-            .iter()
-            .fold(AaBoundingBox::default(), |b1, b2| b1.merge(b2));
-
-        let max_width = curr_bounds.ax_max - curr_bounds.ax_min;
-
-        // check if axis with maximum width has better score than threshold
-        if max_width.x > max_width.y && max_width.x > max_width.z {
-            if scores[0] < threshold {
-                split_axis = 0;
-            }
-        } else if max_width.y > max_width.z {
-            if scores[1] < threshold {
-                split_axis = 1;
-            }
-        } else if scores[2] < threshold {
-            split_axis = 2;
-        }
-
-        // otherwise take the axis with the best score
-        if split_axis == 3 {
-            if scores[0] < scores[1] && scores[0] < scores[2] {
-                split_axis = 0;
-            } else if scores[1] < scores[2] {
-                split_axis = 1;
-            } else {
-                split_axis = 2;
-            }
-        }
-
-        let median = med[split_axis];
-        let partition = |axis: usize, median: f64| {
-            let mut left = Vec::new();
-            let mut right = Vec::new();
-            aabbs.iter().zip(indices).for_each(|(aabb, idx)| {
-                if aabb.ax_min.to_array()[axis] <= median {
-                    left.push(idx);
-                }
-                if aabb.ax_max.to_array()[axis] >= median {
-                    right.push(idx);
-                }
-            });
-            (left, right)
-        };
-
-        // split among the chosen axis
-        let (left, right) = partition(split_axis, median);
-
-        Box::new(Self::Split(
-            split_axis,
-            median,
-            Self::construct(objects, bounds, left),
-            Self::construct(objects, bounds, right),
-        ))
-*/
     }
 }
