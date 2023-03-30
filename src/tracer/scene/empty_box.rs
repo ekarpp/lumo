48f080a224eed87c20192273ad0f00c7e571dca1
ekarpp <eetu@karppinen.xyz>
2023-03-30 17:52:31 +0300

    Revert "move empty box from planes to rectangles to prevent escaping rays"
    This reverts commit acc234026a616cf047663365954940dfd1c08f5c.

--- src/tracer/scene/empty_box.rs
+++ src/tracer/scene/empty_box.rs
@@ -27,88 +27,47 @@ impl Scene {
         let light_dim = 0.4;
 
         let mut scene = Self::default();
-        scene.add(
-            /* rectangular area light */
-            Rectangle::new(
-                DMat3::from_cols(
-                    DVec3::new(-light_dim, -ground - EPSILON, 0.5 * front + light_dim),
-                    DVec3::new(-light_dim, -ground - EPSILON, 0.5 * front - light_dim),
-                    DVec3::new(light_dim, -ground - EPSILON, 0.5 * front - light_dim),
-                ),
-                Material::Light(Texture::Solid(srgb_to_linear(255, 255, 255))),
+        scene.add(/* rectangular area light */ Rectangle::new(
+            DMat3::from_cols(
+                DVec3::new(-light_dim, -ground - EPSILON, 0.5 * front + light_dim),
+                DVec3::new(-light_dim, -ground - EPSILON, 0.5 * front - light_dim),
+                DVec3::new(light_dim, -ground - EPSILON, 0.5 * front - light_dim),
+            ),
+            Material::Light(Texture::Solid(srgb_to_linear(255, 255, 255))),
         ));
 
         scene.add(
             /* floor */
-            Rectangle::new(
-                DMat3::from_cols(
-                    DVec3::NEG_X * right + DVec3::Y * ground + DVec3::NEG_Z * front,
-                    DVec3::X * right + DVec3::Y * ground + DVec3::NEG_Z * front,
-                    DVec3::X * right + DVec3::Y * ground + DVec3::Z * front,
-                ),
-                mat_floor,
-            )
+            Plane::new(DVec3::Y * ground, DVec3::Y, mat_floor),
         );
 
         scene.add(
             /* left wall */
-            Rectangle::new(
-                DMat3::from_cols(
-                    DVec3::NEG_X * right + DVec3::Y * ground + DVec3::NEG_Z * front,
-                    DVec3::NEG_X * right + DVec3::Y * ground + DVec3::Z * front,
-                    DVec3::NEG_X * right + DVec3::NEG_Y * ground + DVec3::Z * front,
-                ),
-                mat_left,
-            )
+            Plane::new(DVec3::NEG_X * right, DVec3::X, mat_left),
         );
 
         scene.add(
             /* right wall */
-            Rectangle::new(
-                DMat3::from_cols(
-                    DVec3::X * right + DVec3::Y * ground + DVec3::Z * front,
-                    DVec3::X * right + DVec3::Y * ground + DVec3::NEG_Z * front,
-                    DVec3::X * right + DVec3::NEG_Y * ground + DVec3::NEG_Z * front,
-                ),
-                mat_right
-            )
+            Plane::new(DVec3::X * right, DVec3::NEG_X, mat_right),
         );
 
-        scene.add(
-            /* roof */
-            Rectangle::new(
-                DMat3::from_cols(
-                    DVec3::NEG_X * right + DVec3::NEG_Y * ground + DVec3::Z * front,
-                    DVec3::X * right + DVec3::NEG_Y * ground + DVec3::Z * front,
-                    DVec3::X * right + DVec3::NEG_Y * ground + DVec3::NEG_Z * front,
-                ),
-                Material::diffuse(Texture::Solid(def_color)),
-            )
-        );
+        scene.add(/* roof */ Plane::new(
+            DVec3::NEG_Y * ground,
+            DVec3::NEG_Y,
+            Material::diffuse(Texture::Solid(def_color)),
+        ));
 
-        scene.add(
-            /* front wall */
-            Rectangle::new(
-                DMat3::from_cols(
-                    DVec3::NEG_X * right + DVec3::Y * ground + DVec3::Z * front,
-                    DVec3::X * right + DVec3::Y * ground + DVec3::Z * front,
-                    DVec3::X * right + DVec3::NEG_Y * ground + DVec3::Z * front,
-                ),
-                Material::diffuse(Texture::Solid(def_color)),
-            )
-        );
+        scene.add(/* front wall */ Plane::new(
+            DVec3::Z * front,
+            DVec3::Z,
+            Material::diffuse(Texture::Solid(def_color)),
+        ));
 
-        scene.add(
-            /* background */
-            Rectangle::new(
-                DMat3::from_cols(
-                    DVec3::X * right + DVec3::Y * ground + DVec3::NEG_Z * front,
-                    DVec3::NEG_X * right + DVec3::Y * ground + DVec3::NEG_Z * front,
-                    DVec3::NEG_X * right + DVec3::NEG_Y * ground + DVec3::NEG_Z * front,
-                ),
-                Material::diffuse(Texture::Solid(srgb_to_linear(0, 0, 0))),
-            ),
-        );
+        scene.add(/* background */ Plane::new(
+            DVec3::NEG_Z * front,
+            DVec3::NEG_Z,
+            Material::diffuse(Texture::Solid(srgb_to_linear(0, 0, 0))),
+        ));
 
         scene
     }
