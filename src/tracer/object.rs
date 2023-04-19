2ce647331b9c656f62aa9d0d11c5cb61a1e92677
ekarpp <eetu@karppinen.xyz>
2023-04-19 08:11:39 +0300

    add possibility to sample leaving rays from sampleable objects

--- src/tracer/object.rs
+++ src/tracer/object.rs
@@ -62,6 +62,19 @@ pub trait Bounded: Object {
 
 /// Object towards which rays can be sampled
 pub trait Sampleable: Object {
+    /// Samples a ray leaving at random point on the surface of the object.
+    /// Direction cos weighed on the hemisphere.
+    fn sample_leaving(&self, rand_sq0: DVec2, rand_sq1: DVec2) -> Ray {
+        let (xo, ng) = self.sample_on(rand_sq0);
+        let uvw = Onb::new(ng);
+        let wi_local = rand_utils::square_to_cos_hemisphere(rand_sq1);
+        let wi = uvw.to_world(wi_local);
+        // pdf start = 1 / area
+        // pdf dir = cos hemisphere
+        // prob want to make sample_leaving_pdf function
+        Ray::new(xo, wi)
+    }
+
     /// Returns randomly sampled point on the surface of the object
     /// and the normal at the point.
     fn sample_on(&self, rand_sq: DVec2) -> (DVec3, DVec3);
