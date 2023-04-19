02dda313d45551b1bf3a9516e0e89a32ec165470
ekarpp <eetu@karppinen.xyz>
2023-04-19 03:57:45 +0300

    update nefertiti scene

--- examples/nefertiti.rs
+++ examples/nefertiti.rs
@@ -3,14 +3,13 @@ use lumo::*;
 use std::f64::consts::PI;
 
 // By CosmoWenmann (https://www.thingiverse.com/thing:3974391) licensed under CC BY-NC-SA 4.0
-const NEFE_URL: &str =
-    "https://cdn.thingiverse.com/assets/c7/e1/b6/f6/12/SPK_Nefertiti_Scan_FOIA_Response.zip";
+const IMAGE_FILE: &str = "examples/aem_aem21300_3dsl01_mo08-03_p_img.png";
+const NEFE_URL: &str = "https://cdn.thingiverse.com/assets/c7/e1/b6/f6/12/SPK_Nefertiti_Scan_FOIA_Response.zip";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let camera = Camera::default();
     let def_color = srgb_to_linear(242, 242, 242);
     let silver = srgb_to_linear(192, 192, 192);
-    let gold = srgb_to_linear(255, 215, 0);
 
     let mut scene = Scene::empty_box(
         def_color,
@@ -21,7 +20,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     scene.add(
         Mesh::new(
             obj::obj_from_url(NEFE_URL)?,
-            Material::metal(Texture::Solid(gold), 0.06),
+            Material::diffuse(Texture::Image(Image::from_file(IMAGE_FILE)?)),
         )
         .to_unit_size()
         .to_origin()
@@ -33,7 +32,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     scene.add(
         Cube::new(Material::specular(
             Texture::Marble(Perlin::new(silver)),
-            0.06,
+            0.04,
         ))
         .translate(-0.5, -0.5, -0.5)
         .scale(0.4, 1.2, 0.4)
