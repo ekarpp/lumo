83c52e47dea95399644acf0755f7639d21f9c48a
ekarpp <eetu@karppinen.xyz>
2023-04-28 00:00:10 +0300

    first draft of watertight triangle intersection

--- src/tracer/object/triangle.rs
+++ src/tracer/object/triangle.rs
@@ -7,10 +7,10 @@ mod triangle_tests;
 pub struct Triangle {
     /// Point `a`
     a: DVec3,
-    /// Point `b` minus `a`
-    b_m_a: DVec3,
-    /// Point `c` minus `a`
-    c_m_a: DVec3,
+    /// Point `b`
+    b: DVec3,
+    /// Point `c`
+    c: DVec3,
     /// Geometric normal. CCW in the order of vertices.
     ng: DVec3,
     /// Shading normal for the vertex `a`
@@ -70,8 +70,8 @@ impl Triangle {
 
         Box::new(Self {
             a,
-            b_m_a,
-            c_m_a,
+            b,
+            c,
             material,
             ng,
             na,
@@ -86,12 +86,9 @@ impl Triangle {
 
 impl Bounded for Triangle {
     fn bounding_box(&self) -> AaBoundingBox {
-        // something better can be done?
-        let b = self.b_m_a + self.a;
-        let c = self.c_m_a + self.a;
         AaBoundingBox::new(
-            self.a.min(b.min(c)),
-            self.a.max(b.max(c)),
+            self.a.min(self.b.min(self.c)),
+            self.a.max(self.b.max(self.c)),
         )
     }
 }
@@ -99,54 +96,73 @@ impl Bounded for Triangle {
 impl Object for Triangle {
     /// Barycentric triangle intersection with Möller-Trumbore algorithm
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<Hit> {
-        /* can cache some results on triangle. */
-        let pde2 = r.dir.cross(self.c_m_a);
-        let det_a = self.b_m_a.dot(pde2);
-        if det_a.abs() < EPSILON {
+        let xo = r.origin;
+        let mut at = self.a - xo;
+        let mut bt = self.b - xo;
+        let mut ct = self.c - xo;
+
+        // need to permute
+        let wi = r.dir;
+
+        let shear = DVec3::new(-wi.x, -wi.y, 0.0) / wi.z;
+
+        at += shear * at.z;
+        bt += shear * bt.z;
+        ct += shear * ct.z;
+
+        let edges = DVec3::new(
+            bt.x * ct.y - bt.y * ct.x,
+            ct.x * at.y - ct.y * at.x,
+            at.x * bt.y - at.y * bt.x,
+        );
+
+        if edges.min_element() < 0.0 && edges.max_element() > 0.0 {
             return None;
         }
 
-        let vec_b = r.origin - self.a;
-
-        let beta = vec_b.dot(pde2) / det_a;
-        if !(0.0..=1.0).contains(&beta) {
+        let det = edges.dot(DVec3::ONE);
+        if det == 0.0 {
             return None;
         }
 
-        let pbe1 = vec_b.cross(self.b_m_a);
-        let gamma = r.dir.dot(pbe1) / det_a;
+        let t_scaled = edges.dot(DVec3::new(at.z, bt.z, ct.z)) * shear.z;
 
-        if gamma < 0.0 || gamma + beta > 1.0 {
+        let b1 = det < 0.0 && (t_scaled > t_min * det || t_scaled < t_max * det);
+        let b2 = det > 0.0 && (t_scaled < t_min * det || t_scaled > t_max * det);
+
+        if b1 || b2 {
             return None;
         }
-        let t = self.c_m_a.dot(pbe1) / det_a;
 
-        if t < t_min + EPSILON || t > t_max {
-            None
-        } else {
-            let alpha = 1.0 - beta - gamma;
+        let alpha = edges.x / det;
+        let beta = edges.y / det;
+        let gamma = edges.z / det;
+        let t = t_scaled / det;
 
-            let ns = alpha * self.na + beta * self.nb + gamma * self.nc;
-            let ns = ns.normalize();
+        let ns = alpha * self.na + beta * self.nb + gamma * self.nc;
+        let ns = ns.normalize();
 
-            let uv = alpha * self.ta + beta * self.tb + gamma * self.tc;
+        let uv = alpha * self.ta + beta * self.tb + gamma * self.tc;
 
-            Hit::new(t, &self.material, r.at(t), ns, self.ng, uv)
-        }
+        Hit::new(t, &self.material, r.at(t), ns, self.ng, uv)
     }
 }
 
 impl Sampleable for Triangle {
     fn area(&self) -> f64 {
-        self.b_m_a.cross(self.c_m_a).length() / 2.0
+        let b_m_a = self.b - self.a;
+        let c_m_a = self.c - self.a;
+        b_m_a.cross(c_m_a).length() / 2.0
     }
 
     /// Random point with barycentrics.
     fn sample_on(&self, rand_sq: DVec2) -> (DVec3, DVec3) {
         let gamma = 1.0 - (1.0 - rand_sq.x).sqrt();
         let beta = rand_sq.y * (1.0 - gamma);
+        let b_m_a = self.b - self.a;
+        let c_m_a = self.c - self.a;
 
-        (self.a + beta * self.b_m_a + gamma * self.c_m_a, self.ng)
+        (self.a + beta * b_m_a + gamma * c_m_a, self.ng)
     }
 
     /// Choose random point on surface of triangle. Shoot ray towards it.
