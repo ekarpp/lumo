636c4b5644eff97975dcbe927e0cb304c659e0ba
ekarpp <eetu@karppinen.xyz>
2023-04-28 10:36:47 +0300

    make cones aligned with y axis

--- src/tracer/object/cone.rs
+++ src/tracer/object/cone.rs
@@ -1,11 +1,7 @@
 use super::*;
 
-/// Cone defined by its tip, height, axis and radius
+/// Cone aligned with the `y` axis and base disk at `y=0`
 pub struct Cone {
-    /// Tip of the cone
-    tip: DVec3,
-    /// Axis or direction of the cone. Normalized in constructor.
-    axis: DVec3,
     /// Height of the cone
     height: f64,
     /// Radius of the circle at the bottom of the cone
@@ -15,11 +11,9 @@ pub struct Cone {
 }
 
 impl Cone {
-    /// Constructs a cone from the given parameters
-    pub fn new(tip: DVec3, axis: DVec3, height: f64, radius: f64, material: Material) -> Box<Self> {
+    /// Constructs a cone from the given `height` and `radius`
+    pub fn new(height: f64, radius: f64, material: Material) -> Box<Self> {
         Box::new(Self {
-            tip,
-            axis: axis.normalize(),
             height,
             radius,
             material,
@@ -30,20 +24,15 @@ impl Cone {
 impl Object for Cone {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<Hit> {
         let xo = r.origin;
-        let tip_to_xo = xo - self.tip;
+        let wi = r.dir;
 
-        let wo = r.dir;
-        let wo_dot_wo = wo.dot(wo);
-        let wo_dot_axis = wo.dot(self.axis);
-        let wo_dot_txo = wo.dot(tip_to_xo);
-        let txo_dot_axis = tip_to_xo.dot(self.axis);
-        let txo_dot_txo = tip_to_xo.dot(tip_to_xo);
+        let tan_theta = self.radius / self.height;
+        let tan2_theta = tan_theta * tan_theta;
+        let xoy_height = xo.y - self.height;
 
-        let tmp = 1.0 + self.radius * self.radius / (self.height * self.height);
-
-        let a = wo_dot_wo - wo_dot_axis.powi(2) * tmp;
-        let b = 2.0 * (wo_dot_txo - wo_dot_axis * txo_dot_axis * tmp);
-        let c = txo_dot_txo - txo_dot_axis.powi(2) * tmp;
+        let a = wi.dot(DVec3::new(wi.x, -tan2_theta * wi.y, wi.z));
+        let b = 2.0 * wi.dot(DVec3::new(xo.x, -tan2_theta * xoy_height, xo.z));
+        let c = xo.x * xo.x - tan2_theta * xoy_height.powi(2) + xo.z * xo.z;
 
         let disc = b * b - 4.0 * a * c;
 
@@ -61,21 +50,18 @@ impl Object for Cone {
         }
 
         let xi = r.at(t);
-        let tip_to_xi = xi - self.tip;
-        let txi_dot_axis = tip_to_xi.dot(self.axis);
 
-        if txi_dot_axis < 0.0 || txi_dot_axis > self.height {
+        if xi.y < 0.0 || xi.y > self.height {
             return None;
         }
 
-        let cos_theta = self.height
-            / (self.height * self.height + self.radius * self.radius).sqrt();
+        let u = xi.x.atan2(xi.z) / 2.0 * PI;
+        let v = xi.y / self.height;
+        let uv = DVec2::new(u, v);
 
-        let a = self.tip + self.axis * tip_to_xi.length() / cos_theta;
-        let ni = (xi - a).normalize();
-
-        let (u, _) = self.axis.any_orthonormal_pair();
-        let uv = DVec2::new(u.dot(xi), txi_dot_axis / self.height);
+        let radius = (xi.x * xi.x + xi.z * xi.z).sqrt();
+        let ni = DVec3::new(xi.x, radius * tan_theta, xi.z);
+        let ni = ni.normalize();
 
         Hit::new(t, &self.material, xi, ni, ni, uv)
     }
