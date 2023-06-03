1c5003bd5058791e941faa4f4c427e09e4a32f84
ekarpp <eetu@karppinen.xyz>
2023-06-03 03:09:50 +0300

    cleanup image struct

--- src/image.rs
+++ src/image.rs
@@ -1,10 +1,8 @@
 use glam::DVec3;
-use png::{BitDepth, ColorType, Decoder, DecodingError, Encoder, EncodingError};
+use png::{BitDepth, ColorType, Decoder, DecodingError};
 use std::fs::File;
-use std::io::BufWriter;
-use std::path::Path;
 
-/// Contains the necessary data to write image buffer to a .png file.
+/// Loaded texture images stored in a Rust vector
 pub struct Image {
     /// Image buffer storing RGB-channels in range \[0,1\].
     pub buffer: Vec<DVec3>,
@@ -15,16 +13,6 @@ pub struct Image {
 }
 
 impl Image {
-    /// Image constructor. Buffer is of length `width * height` with linear
-    /// RGB values in range \[0,1\].
-    pub fn new(buffer: Vec<DVec3>, width: i32, height: i32) -> Self {
-        Self {
-            buffer,
-            width: width as u32,
-            height: height as u32,
-        }
-    }
-
     /// Creates an `image` struct from a file at `path`
     pub fn from_path(path: &str) -> Result<Self, DecodingError> {
         println!("Decoding \"{}\"", path);
@@ -62,39 +50,5 @@ impl Image {
             width,
             height,
         })
-    }
-
-    /// Translates the image buffer of RGB values in range \[0,1\]
-    /// to discrete range \[0,255\]. Applies gamma correction.
-    fn rgb(&self) -> Vec<u8> {
-        self.buffer
-            .iter()
-            .flat_map(|px: &DVec3| {
-                [
-                    self.lin_to_srgb(px.x),
-                    self.lin_to_srgb(px.y),
-                    self.lin_to_srgb(px.z),
-                ]
-            })
-            .collect()
-    }
-
-    /// 2.2 Gamma encodes the linear RGB channel
-    fn lin_to_srgb(&self, c: f64) -> u8 {
-        (c.powf(1.0 / 2.2) * 255.0) as u8
-    }
-
-    /// Creates the PNG file
-    pub fn save(&self, fname: &str) -> Result<(), EncodingError> {
-        println!("Saving to \"{}\"", fname);
-        let path = Path::new(fname);
-
-        let mut binding = BufWriter::new(File::create(path)?);
-        let mut encoder = Encoder::new(&mut binding, self.width, self.height);
-        encoder.set_color(ColorType::Rgb);
-        encoder.set_depth(BitDepth::Eight);
-
-        encoder.write_header()?.write_image_data(&self.rgb())?;
-        Ok(())
     }
 }
